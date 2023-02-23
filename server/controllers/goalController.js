4811eb0e398d7ab36aadcd5fff9c3bea1112f562
cindychau1 <cindyxchau1@gmail.com>
2023-02-23 17:28:34 -0500

    updated routes/controllers configured with frontend

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -20,9 +20,7 @@ const createErr = (errInfo) => {
 const goalController = {
   getAllGoals: async (req, res, next) => {
     try {
-      const allGoals = await prisma.goal.findMany({
-        // where: { title: true }
-      });
+      const allGoals = await prisma.goal.findMany({});
       res.locals.allGoals = allGoals;
       return next();
     } catch (err) {
@@ -33,13 +31,42 @@ const goalController = {
     }
   },
 
+  getUserGoals: async (req, res, next) => {
+    try {
+      // Check if session is valid and associated with user
+      // const session = sessions.get(req.cookies.session);
+      // const userId = req.query.user_id;
+      // console.log({ session, userId });
+      // if (!session) {
+      //   res.status(401).send('Unauthorized');
+      //   return;
+      // }
+      // if (session.userId !== Number(userId)) {
+      //   res.status(403).send('Forbidden');
+      //   return;
+      // }
+      const { userID } = req.params;
+      const userGoals = await prisma.goal.findMany({
+        where: { userId: userID }
+      });
+      res.locals.userGoals = userGoals;
+      return next();
+    } catch (err) {
+      // if DB error, catch that error and return to global error handler.
+      return next(
+        createErr({ method: 'getUserGoals', type: 'getting iser goals', err })
+      );
+    }
+  },
+
   createGoal: async (req, res, next) => {
     try {
       // destructure whatever is necessary from req.body for new path creation.
-      const { title, userId } = req.body;
+      const { title } = req.body;
+      const { userID } = req.params;
       // create new path object here according to DB Schema.
       const newGoal = await prisma.goal.create({
-        data: { title: title, userId: Number(userId) }
+        data: { title: title, userId: userID }
       });
       res.locals.newGoal = newGoal;
       next();
@@ -53,15 +80,13 @@ const goalController = {
 
   updateGoal: async (req, res, next) => {
     try {
-      let { id } = req.params;
-      const NumberId = Number(id);
+      let { cardID } = req.params;
+      const NumberId = Number(cardID);
       // destructure whatever is needed from the req.body to update the path;
       const { title } = req.body;
       const updatedGoal = await prisma.goal.update({
         where: { id: NumberId },
-        data: {
-          title: title
-        }
+        data: { title: title }
       });
       // update existing goal in the DB, the goal id will be passed in the request params (URL);
       res.locals.updatedGoal = updatedGoal;
@@ -75,8 +100,8 @@ const goalController = {
 
   deleteGoal: async (req, res, next) => {
     try {
-      let { id } = req.params;
-      const NumberId = Number(id);
+      let { cardID } = req.params;
+      const NumberId = Number(cardID);
       const deletedGoal = await prisma.goal.delete({
         where: {
           id: NumberId
@@ -93,33 +118,3 @@ const goalController = {
 };
 
 module.exports = goalController;
-
-// get an individual goal and all associated data.
-// getUserGoals: async (req, res, next) => {
-//   try {
-//     // Check if session is valid and associated with user
-//     const session = sessions.get(req.cookies.session);
-//     const userId = req.query.user_id;
-//     console.log({ session, userId });
-//     if (!session) {
-//       res.status(401).send('Unauthorized');
-//       return;
-//     }
-//     if (session.userId !== Number(userId)) {
-//       res.status(403).send('Forbidden');
-//       return;
-//     }
-//     const userGoals = await prisma.goal.findMany({
-//       where: {
-//         userId: Number(session.userId)
-//       }
-//     });
-//     res.locals.userGoals = userGoals;
-//     return next();
-//   } catch (err) {
-//     // if DB error, catch that error and return to global error handler.
-//     return next(
-//       createErr({ method: 'getUserGoals', type: 'getting iser goals', err })
-//     );
-//   }
-// },
